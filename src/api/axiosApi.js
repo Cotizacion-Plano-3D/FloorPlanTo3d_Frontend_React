9b78c7147749964d0883f453e7109be523d0b2e2
Alvaro Sonco <164678960+alvaroSG34@users.noreply.github.com>
2025-10-05 16:20:13 -0400

    Remove all project files
    Deleted all source code. Vamos a migrar todo a NEXT

--- src/api/axiosApi.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-import { API_URL } from "./config";
-
-export const apiClient = axios.create({
-  baseURL: API_URL,
-});
--- src/api/config.js
+++
@@ -1,4 +0,0 @@
-// Centralized API URL configuration
-export const API_URL = "http://127.0.0.1:8000";
-
-//ACa vamos a cambiar la URL cuando subamos el backend a una nube
